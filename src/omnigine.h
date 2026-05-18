e1a784ef8e1a10197deafb0f8be6042e76bdf5d3
RivelBop <rivelbop@gmail.com>
2026-05-18 12:17:43 -0400

    Added Comments to WIDTH and HEIGHT Constants

--- src/omnigine.h
+++ src/omnigine.h
@@ -1,7 +1,9 @@
 #pragma once
 
 namespace Omni {
+    // The default, ideal window width
     extern const int WIDTH;
+    // The default, ideal window height
     extern const int HEIGHT;
 
 #include "raylib.h"
