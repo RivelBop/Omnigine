34d9b0e84188c6e09e1f6bf18a50eb428a8db192
RivelBop <rivelbop@gmail.com>
2026-05-29 02:09:18 -0400

    Integrated SDL3 Example

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,57 @@
-#include "omnigine/omni_main.h"
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-void Init() {
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = nullptr;
+static SDL_Renderer *renderer = nullptr;
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-bool Render(float dt) {
-    BeginDrawing();
-        ClearBackground(BLACK);
-        DrawText("Hello World!", 0, 0, 52, WHITE);
-    EndDrawing();
-    return true;
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
-void Dispose() {
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    static Uint64 start = SDL_GetTicksNS();
+    Uint64 end = SDL_GetTicksNS();
+    float dt = (end - start) / 1e9f;
+    start = end;
+
+    /* clear the window to the draw color. */
+    SDL_RenderClear(renderer);
+
+    /* put the newly-cleared rendering on the screen. */
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;  /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
 }
