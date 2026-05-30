e189ea2fdc73349c842b294971171080fa885f4f
RivelBop <rivelbop@gmail.com>
2026-05-29 21:24:43 -0400

    Added Game-Loop Related Getters in Omni Header

--- src/omnigine/omni.h
+++ src/omnigine/omni.h
@@ -1,8 +1,17 @@
 #pragma once
 
 namespace Omni {
+    /* ==================== WINDOW ==================== */
+
     // The default, ideal window width
     extern const int WIDTH;
     // The default, ideal window height
     extern const int HEIGHT;
+
+    /* ==================== GAME LOOP ==================== */
+
+    // Returns the time it took for the previous frame to complete and start the next frame
+    float DeltaTime();
+    // Returns frames per second
+    int FPS();
 }
