f49aa7bb40f1ebf9d05efcd277a96db6327184cc
RivelBop <rivelbop@gmail.com>
2026-05-30 22:53:09 -0400

    Removed Ideal Window Dimensions and Added Viewport Code to Omni Header

--- src/omnigine/omni.h
+++ src/omnigine/omni.h
@@ -4,22 +4,34 @@
 #include <SDL3/SDL_video.h>
 
 namespace Omni {
-    /* ==================== WINDOW ==================== */
+    /* ==================== WINDOW AND RENDERING ==================== */
 
-    // The default, ideal window width
-    extern const int WIDTH;
-    // The default, ideal window height
-    extern const int HEIGHT;
+    inline constexpr SDL_RendererLogicalPresentation SCREEN_VIEWPORT        = SDL_LOGICAL_PRESENTATION_DISABLED;
+    inline constexpr SDL_RendererLogicalPresentation STRETCH_VIEWPORT       = SDL_LOGICAL_PRESENTATION_STRETCH;
+    inline constexpr SDL_RendererLogicalPresentation FIT_VIEWPORT           = SDL_LOGICAL_PRESENTATION_LETTERBOX;
+    inline constexpr SDL_RendererLogicalPresentation FILL_VIEWPORT          = SDL_LOGICAL_PRESENTATION_OVERSCAN;
+    inline constexpr SDL_RendererLogicalPresentation PIXEL_PERFECT_VIEWPORT = SDL_LOGICAL_PRESENTATION_INTEGER_SCALE;
 
-    // Access to the window pointer
-    SDL_Window *Window();
-    // Access to the renderer pointer
-    SDL_Renderer *Renderer();
+    /** Access to the window pointer. */
+    [[nodiscard]] SDL_Window *Window();
+    /** Access to the renderer pointer. */
+    [[nodiscard]] SDL_Renderer *Renderer();
+
+    /** Calls SDL_SetRenderLogicalPresentation() for the global Renderer(). */
+    inline bool SetViewport(const int width, const int height, const SDL_RendererLogicalPresentation viewport) {
+        return SDL_SetRenderLogicalPresentation(Renderer(), width, height, viewport);
+    }
+
+    /** Calls SDL_SetRenderLogicalPresentation() for the specified SDL_Renderer. */
+    inline bool SetViewport(SDL_Renderer *renderer, const int width, const int height,
+        const SDL_RendererLogicalPresentation viewport) {
+        return SDL_SetRenderLogicalPresentation(renderer, width, height, viewport);
+    }
 
     /* ==================== GAME LOOP ==================== */
 
-    // Returns the time it took for the previous frame to complete and start the next frame
-    float DeltaTime();
-    // Returns frames per second
-    int FPS();
+    /** Returns the time it took for the previous frame to complete and start the next frame. */
+    [[nodiscard]] float DeltaTime();
+    /** Returns frames per second. */
+    [[nodiscard]] int FPS();
 }
