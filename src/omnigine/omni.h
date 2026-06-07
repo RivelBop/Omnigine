406f61bb739e33c4956bb20c7827102d674a6553
RivelBop <rivelbop@gmail.com>
2026-06-07 13:39:40 -0400

    Relocated Window and Renderer in Omni Header

--- src/omnigine/omni.h
+++ src/omnigine/omni.h
@@ -4,7 +4,14 @@
 #include <SDL3/SDL_video.h>
 
 namespace Omni {
-    /* ==================== WINDOW AND RENDERING ==================== */
+    /* ==================== WINDOW & RENDERER ==================== */
+
+    /** Access to the window pointer. */
+    [[nodiscard]] SDL_Window *Window();
+    /** Access to the renderer pointer. */
+    [[nodiscard]] SDL_Renderer *Renderer();
+
+    /* ==================== VIEWPORT ==================== */
 
     using Viewport = SDL_RendererLogicalPresentation;
     inline constexpr Viewport SCREEN_VIEWPORT        = SDL_LOGICAL_PRESENTATION_DISABLED;
@@ -12,11 +19,6 @@ namespace Omni {
     inline constexpr Viewport FIT_VIEWPORT           = SDL_LOGICAL_PRESENTATION_LETTERBOX;
     inline constexpr Viewport FILL_VIEWPORT          = SDL_LOGICAL_PRESENTATION_OVERSCAN;
     inline constexpr Viewport PIXEL_PERFECT_VIEWPORT = SDL_LOGICAL_PRESENTATION_INTEGER_SCALE;
-
-    /** Access to the window pointer. */
-    [[nodiscard]] SDL_Window *Window();
-    /** Access to the renderer pointer. */
-    [[nodiscard]] SDL_Renderer *Renderer();
 
     /** Calls SDL_SetRenderLogicalPresentation() for the global Renderer(). */
     inline bool SetViewport(const int width, const int height, const Viewport viewport) {
