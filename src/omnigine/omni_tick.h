2c36bf18771f0f12df612c3843052d4a34fd11be
RivelBop <rivelbop@gmail.com>
2026-06-24 21:02:09 -0400

    Added Lerp to Omni Tick Header

--- src/omnigine/omni_tick.h
+++ src/omnigine/omni_tick.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <SDL3/SDL_rect.h>
+
 #define OMNI_TICK(clock, dt, rate, logic, alpha) \
     clock += dt;                                 \
     while (clock >= rate) {                      \
@@ -7,3 +9,20 @@
         clock -= rate;                           \
     }                                            \
     alpha = clock / rate;
+
+namespace Omni
+{
+
+inline float Lerp(float start, float end, float alpha)
+{
+    return start + (end - start) * alpha;
+}
+
+inline SDL_FPoint Lerp(SDL_FPoint start, const SDL_FPoint &end, float alpha)
+{
+    start.x += (end.x - start.x) * alpha;
+    start.y += (end.y - start.y) * alpha;
+    return start;
+}
+
+} // namespace Omni
